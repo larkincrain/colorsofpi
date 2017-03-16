948e3d1892fde28cf1ce0f54038d884a797d1a97
larkincrain <larkincrain@gmail.com>
2017-03-16 11:32:23 +0300

    Added home view, added angular app and controller

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+// public/js/app.js
+angular.module('complimentCalendar', 
+	['ngRoute'])
+
+.config ( function ( $routeProvider, $locationProvider) {
+
+    $routeProvider
+
+        // home page
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'MainController'
+        });
+
+    $locationProvider.html5Mode(true);
+
+})
+
+.run( function($window) {
+
+    //check to see if we are still logged in
+
+})
+
+.controller('MainController', function($scope, $window, $location) {
+
+    $scope.email = '';
+    $scope.password = '';
+    $scope.startDigit = 1;
+
+    
+
+});
